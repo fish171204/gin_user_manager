a594ab3f0ebf369eb9584518da3910a26b8bd072
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-06 21:13:40 +0700

    inject service into user handler

---
+++ internal/handler/user_handler.go
@@ -0,0 +1,13 @@
+package handler
+
+import "user-management-api/internal/service"
+
+type UserHandler struct {
+	service *service.UserService
+}
+
+func NewUserHandler(service *service.UserService) *UserHandler {
+	return &UserHandler{
+		service: service,
+	}
+}
