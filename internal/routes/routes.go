97763933fca32d4ec37526052a32d5b6fa1d55ea
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-09 19:08:02 +0700

    feat(router): register middlewares in gin router with correct order

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -11,7 +11,13 @@ type Route interface {
 }
 
 func RegisterRoutes(r *gin.Engine, routes ...Route) {
-	r.Use(middleware.AuthMiddleware())
+	r.Use(
+		middleware.AuthMiddleware(),
+		middleware.LoggerMiddleware(),
+		middleware.ApiKeyMiddleware(),
+		middleware.RateLimiterMiddleware(),
+	)
+
 	api := r.Group("/api/v1")
 
 	for _, route := range routes {
