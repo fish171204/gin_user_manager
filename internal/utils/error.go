4aa0d734ab898bfefa98aed2dd2d55e2b5d5e5e6
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-10 20:23:18 +0700

    feat(utils): add responseError function to return standardized JSON error

--- internal/utils/error.go
+++ internal/utils/error.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type ErrorCode string
@@ -36,6 +38,28 @@ func WrapError(message string, code ErrorCode, err error) error {
 		Code:    code,
 		Err:     err,
 	}
+}
+
+func ResponseError(ctx *gin.Context, err error) {
+	if appError, ok := err.(*AppError); ok {
+		status := httpStatusFromCode(appError.Code)
+		response := gin.H{
+			"error": appError.Message,
+			"code":  appError.Code,
+		}
+
+		if appError.Err != nil {
+			response["detail"] = appError.Err.Error()
+		}
+
+		ctx.JSON(status, response)
+		return
+	}
+
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": err.Error(),
+		"code":  ErrCodeInternal,
+	})
 }
 
 func httpStatusFromCode(code ErrorCode) int {
