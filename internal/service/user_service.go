ce1eb9be1e3ce8d34f93fa8149c6a43a125b32a0
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-12 18:30:46 +0700

    feat(service): implement case-insensitive search with strings.Contains

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -28,7 +28,17 @@ func (us *userService) GetAllUsers(search string, page, limit int) ([]models.Use
 
 	var filteredUsers []models.User
 	// Search
-	 else {
+	if search != "" {
+		search = strings.ToLower(search)
+		for _, user := range users {
+			name := strings.ToLower(user.Name)
+			email := strings.ToLower(user.Email)
+
+			if strings.Contains(name, search) || strings.Contains(email, search) {
+				filteredUsers = append(filteredUsers, user)
+			}
+		}
+	} else {
 		// No search results
 		filteredUsers = users
 	}
