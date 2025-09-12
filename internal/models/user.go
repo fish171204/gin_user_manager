5c2a8f38443786f69dee05ea3d2df204bb90da79
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-12 21:03:05 +0700

    refactor (user_models)

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	UUID     string `json:"uuid" binding:"required,uuid"`
+	UUID     string `json:"uuid"`
 	Name     string `json:"name" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
 	Age      int    `json:"age" binding:"required,gt=18"`
