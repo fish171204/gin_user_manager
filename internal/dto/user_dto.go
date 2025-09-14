769efcd9b3ab7594d376e273692cc7fbf599bfe0
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-14 18:51:42 +0700

    feat(dto):separate Model and DTO for validation management

--- internal/dto/user_dto.go
+++ internal/dto/user_dto.go
@@ -12,6 +12,16 @@ type UserDTO struct {
 	Level  string `json:"level"`
 }
 
+type CreateUserInput struct {
+	UUID     string `json:"uuid"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Age      int    `json:"age" binding:"required,gt=18"`
+	Password string `json:"password" binding:"required,min=8,password_strong"`
+	Status   int    `json:"status" binding:"required,oneof=1 2"`
+	Level    int    `json:"level" binding:"required,oneof=1 2"`
+}
+
 func MapUserToDTO(user models.User) *UserDTO {
 	return &UserDTO{
 		UUID:   user.UUID,
