a76de88a9876478ee14c9770fa60d51fc7586f86
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-14 20:06:11 +0700

    feat(utils): add convert updateStringField func and UpdateIntField func

--- internal/utils/convert.go
+++ internal/utils/convert.go
@@ -12,10 +12,22 @@ var (
 
 func CamelToSnake(str string) string {
 	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
-	snake = matchAllCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
 	return strings.ToLower(snake)
 }
 
 func NormalizeString(text string) string {
 	return strings.ToLower(strings.TrimSpace(text))
+}
+
+func UpdateStringField(current *string, new string) {
+	if new != "" {
+		*current = new
+	}
+}
+
+func UpdateIntField(current *int, new int) {
+	if new > 0 {
+		*current = new
+	}
 }
